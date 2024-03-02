a1afa74a032574637934f11575a6ff8b99f286ad
dmichalc <98069002+dmichalc@users.noreply.github.com>
2024-03-01 17:38:17 -0700

    added body

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,18 @@
 export default function HomePage() {
-  return <h1>Hello, world!</h1>;    
+  return (
+  <><main>
+    <h1>Hello, world!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      In et tempus ligula. Phasellus quis feugiat mauris, at vulputate 
+      mi. Nam semper hendrerit velit, sed sollicitudin sapien pharetra 
+      nec. Proin eget urna in odio aliquet fermentum vel ut ante. 
+      In sed volutpat odio. Maecenas faucibus consectetur magna. 
+      Quisque faucibus cursus tortor. Pellentesque ex est, posuere 
+      et neque convallis, dapibus euismod velit. Aenean tempor id leo 
+      at tincidunt. Sed at nunc finibus, pulvinar elit a, efficitur 
+      velit. Pellentesque lacinia ipsum eu ultrices varius.</p>
+    
+    </main> 
+    <footer></footer>
+    </> )
 }
